5748ee32b3cdd9d76cb103317220b22ea56078d8
akshar <takleakshar@gmail.com>
2018-04-14 11:46:09 -0700

    employee

---
+++ server/actions.js
@@ -0,0 +1,20 @@
+const { Client } = require("pg");
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+function addEmployee(name, base_salary, emp_id) {
+  client.query(
+    `INSERT INTO employees (name, base_salary,emp_id) VALUES ('${name}','${base_salary}','${emp_id}')`,
+    (err, result) => {
+      if (err) {
+        return console.log(result);
+      } else {
+        console.log("added");
+      }
+    }
+  );
+}
+
+module.exports = addEmployee;
---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const shortid = require("shortid");
+const app = express();
+//cors middleware
+app.use(cors());
+//require database add function
+const addEmployee = require("./actions");
+//set static folders
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../client/dist"));
+app.use(express.static(path.join(__dirname, "../client/dist")));
+//initialize database
+const { Client } = require("pg");
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+//create tables
+const db_creation_string = `CREATE TABLE IF NOT EXISTS employees(id SERIAL PRIMARY KEY, name TEXT, base_salary TEXT, emp_id TEXT);`;
+
+//body parser
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  client.query(db_creation_string, (err, res) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("done" + res);
+    }
+  });
+  res.render("index.ejs");
+});
+app.post("/add", (req, res) => {
+  let shortId = shortid.generate();
+  let name = req.body.employeeName;
+  let baseSalary = req.body.baseSalary;
+  let deductions = req.body.deductions;
+  addEmployee(name, baseSalary, shortId);
+});
+app.listen(process.env.PORT, () => console.log("listening"));
